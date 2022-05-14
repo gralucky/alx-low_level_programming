2d6f0ebfbf08bcee7f7e3dd982225c3f83aa3332
gralucky <mykythuo18@gmail.com>
2022-05-14 01:53:21 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-dog_t *new_dog(char *Poppy, float 3.5, char *Bob);
+dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
