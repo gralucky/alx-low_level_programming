a9caadcbb484bace7eb11075fb27e11ab2d7a6a6
gralucky <mykythuo18@gmail.com>
2022-06-17 05:36:31 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @filename: file to append the next to
  * @text_content: content to append into the file
  *
- * Return: 1 0n success and -1 on failure
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
